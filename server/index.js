40e144716bf2734eaf41e015174f327daf397fec
Miguel Villanueva <emvil542@gmail.com>
2023-02-14 05:00:22 +0800

    feat: add chats layout

--- server/index.js
+++ server/index.js
@@ -44,103 +44,13 @@ app.get("/", (request, response) => {
 	}
 )
 
-app.get("/greeting", (request, response) => {
-	response.send("Hello from B230-Malinao");
+app.get("/users", (request, response) => {
+	response.send("Hello from my first ExpressJSAPI");
 	}
 )
 
-// retrieval of users in mock database
-app.get("/users", (req, res) => {
-	res.send(users);
-})
-
-app.post("/users" , (request, response) =>{
-
-	let newUser = {
-		username: request.body.username,
-		password: request.body.password
-	}
-
-	users.push(newUser);
-	console.log(users);
-
-	response.send(users);
-})
-
-app.put("/users/:index", (req,res) => {
-
-	console.log(req.body);
-
-	console.log(req.params); // index: 1
-	let index = parseInt(req.params.index); // '1' >> 1
-
-	users[index].username = req.body.username;
-	users[index].password = req.body.password;
-
-	res.send(users[index]); 
-
-})
-
-// Delete the last element in an array
-app.delete('/users', (req, res) => {
-	users.pop();
-	res.send(users);
-});
 
 
-//-------------
-
-let items = [
-		{
-			name: "Mjolnir",
-			price: 50000,
-			isActive: true
-		},
-		{
-			name: "Vibranium Shield",
-			price: 70000,
-			isActive: true
-		}
-
-]
-
-app.get("/items", (request, response) => {
-	response.send(items);
-})
-
-    app.post("/items" , (request, response) =>{
-
-        let newItem = {
-			name: request.body.name,
-			price: request.body.price,
-			isActive: request.body.isActive,
-        }
-    
-        items.push(newItem);
-
-        console.log(items);
-    
-        response.send(items);
-    });
-
-app.put("/items/:index", (request, response) => {
-
-	let index = parseInt(request.params.index);
-
-	items[index].price = request.body.price;
-
-	response.send(items[index]); 
-
-});
-
-app.delete("/items/:index", (request, response) => {
-
-	let index = parseInt(request.params.index);
-
-    items = items.filter(item => items.indexOf(item) !== index);
-
-    response.send(items);
-});
 
 
 app.listen(port, () => console.log(`Server is running at port ${port}`));
